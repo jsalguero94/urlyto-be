7747d4459022425408670626881bc5342e4ddef8
jsalguero94 <jsalguero94@gmail.com>
2025-05-06 22:54:25 -0600

    feat: Add Helmet

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import helmet from '@fastify/helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -11,6 +12,7 @@ async function bootstrap() {
     new FastifyAdapter({ logger: true }),
   );
 
+  await app.register(helmet);
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
