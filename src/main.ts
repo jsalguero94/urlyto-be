e3c1c4a084d7450195067c6d2b52e79ea8391dac
Julio A. Salguero <jsalguero94@gmail.com>
2025-05-06 22:57:52 -0600

    Feature/skeleton (#1)
    * feat: nest new --strict
    
    * feat: Add Fastify
    
    * feat: Add Helmet

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+import helmet from '@fastify/helmet';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true }),
+  );
+
+  await app.register(helmet);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+}
+bootstrap();
